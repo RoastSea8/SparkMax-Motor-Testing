d63ed26fb315972f7958d0a72d4e017582cdaa39
aditya2604 <aditya26042005@gmail.com>
2021-07-30 02:26:17 -0700

    fixed old motor testing code

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -5,169 +5,49 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-#include <frc/TimedRobot.h>
-#include <frc/smartdashboard/SmartDashboard.h>
+#include "Robot.h"
 #include "rev/CANSparkMax.h"
-#include <iostream> 
-#include <fstream>
 
 using namespace std; 
 
-// Create a file called adityaFile
-static ofstream adityaFile;
-
-class Robot : public frc::TimedRobot {
-  /**
-   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
-   * 
-   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
-   * first parameter
-   * 
-   * The motor type is passed as the second parameter. Motor type is set to Brushless motor.
-   * 
-   * Initializes four brushless motors with CAN IDs 1, 2, 3 and 4.
-   */
-  static const int leftLeadDeviceID = 3, rightLeadDeviceID = 13; //, leftFollowDeviceID = 2, rightFollowDeviceID = 4;
-  rev::CANSparkMax* m_leftLeadMotor = new rev::CANSparkMax(leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless);
-  rev::CANSparkMax* m_rightLeadMotor = new rev::CANSparkMax(rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless);
-  //rev::CANSparkMax* m_leftFollowMotor = new rev::CANSparkMax(leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless);
-  //rev::CANSparkMax* m_rightFollowMotor = new rev::CANSparkMax(rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless);
-
-  /**
-   * A CANAnalog object is constructed using the GetAnalog() method on an 
-   * existing CANSparkMax object. 
-   */
-  rev::CANEncoder m_encoderSensor_left_motor = m_leftLeadMotor->GetEncoder();
-  rev::CANEncoder m_encoderSensor_right_motor = m_rightLeadMotor->GetEncoder();
-
-  /**
-   * In RobotInit() below, we will configure m_leftFollowMotor and m_rightFollowMotor to follow 
-   * m_leftLeadMotor and m_rightLeadMotor, respectively. 
-   * 
-   * Because of this, we only need to pass the lead motors to m_robotDrive. Whatever commands are 
-   * sent to them will automatically be copied by the follower motors
-   */
+void Robot::RobotInit() {
+  m_leftLeadMotor->RestoreFactoryDefaults();
+  m_rightLeadMotor->RestoreFactoryDefaults();
+  m_leftFollowMotor->RestoreFactoryDefaults();
+  m_rightFollowMotor->RestoreFactoryDefaults();
   
-  void Move(double leftMotorSpeed, double rightMotorSpeed) {
-    int direction; 
-    // checking if both motor speeds are in range: between or equal to -1 and 1
-    if (((leftMotorSpeed >= -1) && (leftMotorSpeed <= 1)) && ((rightMotorSpeed >= -1) && (rightMotorSpeed <= 1))) {
-      if (leftMotorSpeed == rightMotorSpeed){
-        if (leftMotorSpeed > 0) {
-          direction = 1; //forward
-        } else if (leftMotorSpeed < 0) {
-          direction = 2; //backward
-        }
-      } 
-      else {
-        if (leftMotorSpeed > rightMotorSpeed) {
-          direction = 4; //right
-        } else {
-          direction = 3; //left
-        }
-      }
-      // setting the speed of the lead motors
-      m_leftLeadMotor->Set(leftMotorSpeed);
-      m_rightLeadMotor->Set(rightMotorSpeed); 
-      // outputting speeds of both motors to file 
-      adityaFile << "\n Expected direction: " << direction << " Left motor speed: " << leftMotorSpeed << " Right motor speed: " << rightMotorSpeed << "\n";
+  m_leftLeadMotor->SetInverted(false);
+  m_leftFollowMotor->Follow(*m_leftLeadMotor);
+  m_rightLeadMotor->SetInverted(true);
+  m_rightFollowMotor->Follow(*m_rightLeadMotor);
 
-      // variable to hold the value of GetVelocity() for the left motor
-      double leftMotorVelocity = m_encoderSensor_left_motor.GetVelocity();
-      // variable to hold the value of GetVelocity() for the right motor
-      double rightMotorVelocity = m_encoderSensor_right_motor.GetVelocity();
+  m_robotDrive = new frc::DifferentialDrive(*m_leftLeadMotor, *m_rightLeadMotor);
 
-      switch (direction) {
-        case 1:
-          // if the velocities of both motors are the same, and if their velocities are greater than 0, then the robot is moving forward
-          if((leftMotorVelocity > 0) && (leftMotorVelocity == rightMotorVelocity)) {  
-            adityaFile << "SUCCESSFULL: Robot moving FORWARD with LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity <<"\n";
-          } else
-          {
-            adityaFile << "ERROR: Robot not moving FORWARD - LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
-            return;
-          }
-          break;
-        case 2: 
-          // if the velocities of both motors are the same, and if their velocities are less than 0, then the robot is moving backward
-          if((leftMotorVelocity < 0) && (leftMotorVelocity == rightMotorVelocity)) {
-            adityaFile << "SUCCESSFULL: Robot moving BACKWARD with LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
-          } else {
-            adityaFile << "ERROR: Robot not moving BACKWARD - LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
-          }
-          break;
-        case 3: 
-          // if the velocity of the right motor is greater than the velocity of the left motor, the robot is turning left
-          if (leftMotorVelocity < rightMotorVelocity) {
-            adityaFile << "SUCCESSFULL: Robot turning LEFT with LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
-          } else {
-            adityaFile << "ERROR: Robot not turning LEFT - LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
-          }
-          break;
-        case 4:
-          // if the velocity of the left motor is greater than the velocity of the right motor, the robot is turning right
-          if (leftMotorVelocity > rightMotorVelocity) {
-            adityaFile << "SUCCESSFULL: Robot turning RIGHT with LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
-          } else {
-            adityaFile << "ERROR: Robot not turning RIGHT - LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
-          }
-          break;
-          default:
-          //Error invalid direction 
-          frc::SmartDashboard::PutString("ERROR: Invalid Direction", "\n");
-        }
-    } else { 
-      // outputting an error if the left or right motor speeds are not in range
-      frc::SmartDashboard::PutString("ERROR: Left motor speed not in range/", "right motor speed not in range");
-    }
-    return;
-  }
+  m_controller = new frc::XboxController{0}; // replace with USB port num on DS
+}
+void Robot::RobotPeriodic() {}
 
-  void StopMotors(){
-    //passing 0 as the parameter to make the motors stop
-    m_leftLeadMotor->Set(0);
-    m_rightLeadMotor->Set(0);
-  } 
- public:
-  void RobotInit() {
-    /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
-     */
-    m_leftLeadMotor->RestoreFactoryDefaults();
-    m_rightLeadMotor->RestoreFactoryDefaults();
-    //m_leftFollowMotor->RestoreFactoryDefaults();
-    //m_rightFollowMotor->RestoreFactoryDefaults();
-    
-    /**
-     * In CAN mode, one SPARK MAX can be configured to follow another. This is done by calling
-     * the Follow() method on the SPARK MAX you want to configure as a follower, and by passing
-     * as a parameter the SPARK MAX you want to configure as a leader.
-     * 
-     * one motor on each side of our drive train is configured to follow a lead motor.
-     */
-    //m_leftFollowMotor->Follow(*m_leftLeadMotor);
-    //m_rightFollowMotor->Follow(*m_rightLeadMotor);
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
 
-    adityaFile.open("/home/lvuser/Output.txt", ios::app); //to append to the next line
-    
-    //moving at half speed
-    Move(0.5, 0.5);   //Forward
-    Move(-0.5, -0.5); //Backward
-    Move(-0.5, 0.5);  //Left - the left motor moves backward and the right motor moves forward
-    Move(0.5, -0.5);  //Right - the left motor moves forward and the right motor moves backward
-    StopMotors(); //stopping the motors
+void Robot::TeleopInit() {}
+void Robot::TeleopPeriodic() {
+  if (m_controller->GetAButton())
+    Move(0.2, 0.2);   // Forward check
+  if (m_controller->GetBButton())
+    Move(-0.2, -0.2); // Backward check
+  if (m_controller->GetYButton())
+    Move(-0.2, 0.2);  // Left check
+  if (m_controller->GetXButton())
+    Move(0.2, -0.2);  // Right check
+}
 
-    //closing the file
-    adityaFile.close();
-  };
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
 
-  void TeleopPeriodic() {
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
 
-  };
-
-};
 #ifndef RUNNING_FRC_TESTS
 int main() { return frc::StartRobot<Robot>(); }
 #endif
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -8,10 +8,31 @@
 #pragma once
 
 #include <frc/TimedRobot.h>
+#include "rev/CANSparkMax.h"
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/XboxController.h>
+#include <string>
+#include <vector>
 
 class Robot : public frc::TimedRobot {
  public:
+  static const int leftLeadDeviceID = 3, rightLeadDeviceID = 13, leftFollowDeviceID = 2, rightFollowDeviceID = 4;
+  rev::CANSparkMax* m_leftLeadMotor = new rev::CANSparkMax(leftLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax* m_rightLeadMotor = new rev::CANSparkMax(rightLeadDeviceID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax* m_leftFollowMotor = new rev::CANSparkMax(leftFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax* m_rightFollowMotor = new rev::CANSparkMax(rightFollowDeviceID, rev::CANSparkMax::MotorType::kBrushless);
+
+  double leftMotorSpeed{0.0}, rightMotorSpeed{0.0}, leftMotorVelocity{0.0}, rightMotorVelocity{0.0};
+  enum Direction {F, B, L, R};
+  std::vector<std::string> d2String {"forward", "backward", "left", "right"};
+  Direction d;
+
+  frc::DifferentialDrive* m_robotDrive;
+
+  frc::XboxController* m_controller;
+
   void RobotInit() override;
+  void RobotPeriodic() override;
 
   void AutonomousInit() override;
   void AutonomousPeriodic() override;
@@ -19,6 +40,80 @@ class Robot : public frc::TimedRobot {
   void TeleopInit() override;
   void TeleopPeriodic() override;
 
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+
   void TestInit() override;
   void TestPeriodic() override;
+
+  bool inRange(double);
+  void Move(double, double);
 };
+
+bool Robot::inRange(double speed) {
+  if ((speed >= -1) && (speed <= 1)) {
+    return true;
+  }
+  else return false;
+}
+
+void Robot::Move(double leftMotorSpeed, double rightMotorSpeed) {
+  m_robotDrive->TankDrive(leftMotorSpeed, rightMotorSpeed);
+
+  if (inRange(leftMotorSpeed) && inRange(rightMotorSpeed)) {
+    if (leftMotorSpeed == rightMotorSpeed) {
+      if (leftMotorSpeed > 0) d = F; else d = B;
+    }
+    else {
+      if (leftMotorSpeed < rightMotorSpeed) d = L; else d = R;
+    }
+    
+    wpi::outs() << "\n Expected direction: " << d2String[d] << ", Left motor speed: " << leftMotorSpeed << ", Right motor speed: " << rightMotorSpeed << "\n";
+
+    leftMotorVelocity = m_leftLeadMotor->GetEncoder().GetVelocity();
+    rightMotorVelocity = m_rightLeadMotor->GetEncoder().GetVelocity();
+
+    switch (d) {
+      case F:
+        // if the velocities of both motors are the same, and if their velocities are greater than 0, then the robot is moving forward
+        if((leftMotorVelocity == rightMotorVelocity) && (leftMotorVelocity > 0))
+          wpi::outs() << "SUCCESSFUL: Robot moving FORWARD with LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity <<"\n";
+        else
+        {
+          wpi::outs() << "ERROR: Robot not moving FORWARD - LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
+          return;
+        }
+        break;
+      case B: 
+        // if the velocities of both motors are the same, and if their velocities are less than 0, then the robot is moving backward
+        if((leftMotorVelocity == rightMotorVelocity) && (leftMotorVelocity < 0))
+          wpi::outs() << "SUCCESSFULL: Robot moving BACKWARD with LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
+        else {
+          wpi::outs() << "ERROR: Robot not moving BACKWARD - LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
+        }
+        break;
+      case L: 
+        // if the velocity of the right motor is greater than the velocity of the left motor, the robot is turning left
+        if (leftMotorVelocity < rightMotorVelocity)
+          wpi::outs() << "SUCCESSFULL: Robot turning LEFT with LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
+        else {
+          wpi::outs() << "ERROR: Robot not turning LEFT - LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
+        }
+        break;
+      case R:
+        // if the velocity of the left motor is greater than the velocity of the right motor, the robot is turning right
+        if (leftMotorVelocity > rightMotorVelocity)
+          wpi::outs() << "SUCCESSFULL: Robot turning RIGHT with LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
+        else {
+          wpi::outs() << "ERROR: Robot not turning RIGHT - LEFT motor velocity = " << leftMotorVelocity << " and RIGHT motor velocity = " << rightMotorVelocity << "\n";
+        }
+        break;
+        default:
+        //Error invalid direction 
+        wpi::outs() << "ERROR: Invalid Direction \n";
+      }
+  } else { 
+    // outputting an error if the left or right motor speeds are not in range
+    wpi::outs() << "ERROR: Left motor speed and right motor speed not in range \n";
+  }
+}
